4bb8076114b88ca9124c4343a6e62deaf0f78e88
GuilhermeSallesBlanco <guilherme.salles.blanco@gmail.com>
2025-04-27 01:19:06 -0300

    Adicionando tarefa realizada

--- numeros_com_sinal.v
+++ numeros_com_sinal.v
@@ -7,6 +7,14 @@ module numeros_com_sinal(
     output reg [7:0] saida
 );
 
-//insira seu codigo aqui
+always @ (codigo) begin // Como a saída é um reg, ela pode ser manipulada em um bloco always.
+    case(codigo)
+        2'b00: saida = entrada_signed_1 + entrada_signed_2; // signed_1 + signed_2
+        2'b01: saida = entrada_unsigned_1 + entrada_unsigned_2; // unsigned_1 + unsigned_2
+        2'b10: saida = entrada_unsigned_1 + entrada_signed_1; // unsigned_1 + signed_1
+        2'b11: saida = entrada_unsigned_1 + entrada_unsigned_2; // unsigned_1 + signed_2
+        default: saida = 8'b00000000; // Zera se código for qualquer outro valor
+    endcase
+end
 
 endmodule
